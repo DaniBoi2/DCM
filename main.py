2f3129010a1016e63f711941a7e26ed3ff09bea5
HassanN01230 <nasirs14@mcmaster.ca>
2023-10-10 00:53:57 -0400

    changes
    deleted admin_window as don't need. We don't need other windows either but Im leaving them in case you guys need to do something with em

--- main.py
+++ main.py
@@ -44,24 +44,24 @@ class Pacemaker_GUI():
                 label_bottom.config(text="All fields must contain content, please try again")   #If all fields are not filled
             elif (not entry_name.get().replace(" ", "").isalpha()):  #Get rid of spaces as they are allowed for full names
                 label_bottom.config(text="Invalid name, only alphabets allowed, please try again")   # If there are non alphabets in the name field
-            elif (num_dicts >=  self.max_users_allowed):    # If max number of users already in json file
+            elif (num_dicts >= self.max_users_allowed):    # If max number of users already in json file
                 label_bottom.config(text="The maximum number of user accounts have already been created")
             elif (len(entry_password.get()) <= 7):   #If password is 7 or less characters
                 label_bottom.config(text="Password must be at least 8 characters, please try again")
             elif (len(entry_name.get()) >= 35):   #Name shouldn't be too long or displaying it in main screen can cause visual errors
                 label_bottom.config(text="Please enter a shorter name and try again")
+            elif (any(entry_username.get() == user.get("username") for user in user_items)):  #Making sure same username can't be used again
+                label_bottom.config(text="That username is not available, please choose another one")
             else:   #Consolidating inputted user data
                 user_items.append({"username": entry_username.get(), "password": entry_password.get(), "name": entry_name.get(), "email": entry_email.get()})
-                
-                with open(self.filename, "w") as user_file:  #Open in write mode, can't append in json
-                    json.dump(user_items, user_file, indent = 4)  #Transfer all contents to json file
+                with open(self.filename, "w") as user_file: 
+                    json.dump(user_items, user_file, indent = 4)  #Open in write mode, can't append in json, transfer all contents
 
                 label_bottom.config(text="User has been registered")
                 entry_name.delete(0, END)
                 entry_email.delete(0, END)      #Make all entry widgets empty
                 entry_username.delete(0, END)
                 entry_password.delete(0, END)
-
                 if os.path.exists("user_items.json"): os.remove("user_items.json")  #Deleting it incase it remains
 
         # Create the window, labels, entry widgets, button, etc.
@@ -95,13 +95,12 @@ class Pacemaker_GUI():
         label_bottom.pack()
     
     def on_register_window_close(self):  #Don't touch, created for closing register_window properly during successful login
-        self.register_window.destroy()
+        self.register_window.destroy()   #To understand why it's needed, look at the test cases written in documentation
         self.register_window_open = False
         self.register_window = None
     
     def login_screen(self):  #Login window pops up
-        with open(self.filename, "r") as file: users = json.load(file)
-
+        with open(self.filename, "r") as file: users = json.load(file)  #Loading contents of json file so they can be read 
         def login():
             if (entry_username.get() == self.admin_u and entry_password.get() == self.admin_p):
                 if self.register_window_open: self.register_window.withdraw()  #Hide register screen if its deemed open
@@ -148,7 +147,7 @@ class Pacemaker_GUI():
         label_bottom.pack()
     
     def admin_screen(self): #Admin window pops up
-        def delete_user_data():
+        def delete_user_data():  #Reads user data, saves it into txt file, and then opens json file in write mode to delete everything
             with open(self.filename, "r") as user_file: user_items = json.load(user_file)  #Open in read mode and load json file and read contents
             with open(self.backup_file, "a") as backup_file:  #Save contents to backup txt file, appends to everything else
                 for user in user_items: backup_file.write(json.dumps(user) + "\n")  # Backup every user data by writing it to txt file
@@ -170,21 +169,22 @@ class Pacemaker_GUI():
         label_bottomName = tk.Label(admin_page, text="Logged in as admin")
         label_bottomName.pack(pady=(20, 0), anchor="n")  # Putting it at bottom 
 
-    def main_screen(self,user_name): #Main screen pops up
+    def main_screen(self,user_name): #Main screen pops up, user_name as input so it can display "Logged in as [user_name]" 
         def logout(): 
             main_window.withdraw()  #At logout, hide main screen window and show welcome screen again
             self.welcome.deiconify()
         def AOO_pressed():
-            label_state.config(text="State: AOO")
+            label_state.config(text="State:  AOO")
         def VOO_pressed():
-            label_state.config(text="State: VOO")
+            label_state.config(text="State:  VOO")
         def AAI_pressed():
-            label_state.config(text="State: AAI")
+            label_state.config(text="State:  AAI")
         def VVI_pressed():
-            label_state.config(text="State: VVI")
+            label_state.config(text="State:  VVI")
+
         main_window = tk.Tk()
         main_window.title("Pacemaker Control Platform")
-        main_window.geometry(f"810x350+{self.x_offset}+{self.y_offset}")  #Displaying it roughly in the center of the user screen
+        main_window.geometry(f"810x400+{self.x_offset}+{self.y_offset}")  #Displaying it roughly in the center of the user screen
         main_window.resizable(False, False)
         label_topLeft =tk.Label(main_window, text="Logged in as " + user_name)
         label_topLeft.grid(row=0, column=0, sticky="nw")  # Putting it at top left
@@ -192,7 +192,7 @@ class Pacemaker_GUI():
         label_top.grid(row=0, column=1, columnspan=2, sticky="n", pady=(0,10))  # Putting it at top
         button_logout = tk.Button(main_window, text="Logout", command=logout)
         button_logout.grid(row=0, column=5, sticky="ne")
-        label_state = tk.Label(main_window, text="State")
+        label_state = tk.Label(main_window, text="State")  #Used to show the current state (AOO,VOO...etc)
         label_state.grid(row=2, column=0, sticky="nw")
 
         # 4 Buttons for AOO,VOO,AAI,VVI
@@ -204,6 +204,36 @@ class Pacemaker_GUI():
         button_AAI.grid(row=2, column=1, sticky="n", padx=65)
         button_VVI = tk.Button(main_window, text="VVI", command=VVI_pressed)
         button_VVI.grid(row=2, column=3, sticky="n", padx=65)
+
+        #Displaying programmable parameters labels in left column
+        label_lower_rate_limit = tk.Label(main_window, text="Lower Rate Limit (units) (range)")
+        label_lower_rate_limit.grid(row=3, column=0, pady=(20,10), padx=(20,0), sticky="w")
+        label_upper_rate_limit = tk.Label(main_window, text="Upper Rate Limit (units) (range)")
+        label_upper_rate_limit.grid(row=4, column=0, pady=10, padx=(20,0), sticky="w")
+        label_atrial_amp = tk.Label(main_window, text="Atrial Amplitude (units) (range)")
+        label_atrial_amp.grid(row=5, column=0, pady=10, padx=(20,0), sticky="w")
+        label_atrial_pulse_width = tk.Label(main_window, text="Atrial Pulse Width (units) (range)")
+        label_atrial_pulse_width.grid(row=6, column=0, pady=10, padx=(20,0), sticky="w")
+        label_ventricular_amp = tk.Label(main_window, text="Ventricular Amplitude (units) (range)")
+        label_ventricular_amp.grid(row=7, column=0, pady=10, padx=(20,0), sticky="w")
+        label_ventricular_pulse_width = tk.Label(main_window, text="Ventricular Pulse Width (units) (range)")
+        label_ventricular_pulse_width.grid(row=8, column=0, pady=10, padx=(20,0), sticky="w")
+        label_ventricular_sensitivity = tk.Label(main_window, text="Ventricular Sensitivity (units) (range)")
+        label_ventricular_sensitivity.grid(row=9, column=0, pady=10, padx=(20,0), sticky="w")
+
+        #Displaying programmable parameters labels in 2nd column
+        label_atrial_sensitivity = tk.Label(main_window, text="Atrial Sensitivity (units) (range)")
+        label_atrial_sensitivity.grid(row=3, column=2, pady=(20,10), padx=(20,0), sticky="w")
+        label_ARP = tk.Label(main_window, text="ARP (units) (range)")
+        label_ARP.grid(row=4, column=2, pady=10, padx=(20,0), sticky="w")
+        label_VRP = tk.Label(main_window, text="VRP (units) (range)")
+        label_VRP.grid(row=5, column=2, pady=10, padx=(20,0), sticky="w")
+        label_PVARP = tk.Label(main_window, text="PVARP (units) (range)")
+        label_PVARP.grid(row=6, column=2, pady=10, padx=(20,0), sticky="w")
+        label_hysteresis = tk.Label(main_window, text="Hysteresis (units) (range)")
+        label_hysteresis.grid(row=7, column=2, pady=10, padx=(20,0), sticky="w")
+        label_rate_smoothing = tk.Label(main_window, text="Rate Smoothing (units) (range)")
+        label_rate_smoothing.grid(row=8, column=2, pady=10, padx=(20,0), sticky="w")
     
 if __name__ == "__main__": #Used to make sure that the program is run directly and that it's not imported anywhere
     pacemaker = Pacemaker_GUI()
